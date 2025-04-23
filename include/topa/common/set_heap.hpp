78b536fa64b4abc8f7465edfb2b10f0ca759563a
Alexia <9613983333@mail.ru>
2025-04-23 14:49:03 +0300

    Implement SetHeap and BitTreeHeap for PosHeap model

---
+++ include/topa/common/set_heap.hpp
@@ -0,0 +1,44 @@
+#pragma once
+
+#include "models/pos_heap.hpp"
+
+#include <set>
+
+namespace topa::common {
+
+class SetHeap : public models::PosHeap<SetHeap> {
+   public:
+    explicit SetHeap(size_t) {
+        //
+    }
+
+    Position GetMaxPos() const;
+    bool IsEmpty() const;
+
+    SetHeap& operator+=(const SetHeap& other);
+
+    template <PositionRange R>
+    void Add(R&& range) {
+        for (const auto& pos : range) {
+            auto it = positions_.find(pos);
+            if (it == positions_.end()) {
+                positions_.insert(pos);
+            } else {
+                positions_.erase(it);
+            }
+        }
+    }
+
+    void Add(std::initializer_list<Position> positions) {
+        Add(std::views::all(positions));
+    }
+
+    Position PopMaxPos();
+    Positions PopAll();
+    void Clear();
+
+   private:
+    std::set<Position> positions_;
+};
+
+}  // namespace topa::common
