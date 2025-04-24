25688b0c768f787b82071a253a7c49b1315bd32c
Alexia <9613983333@mail.ru>
2025-04-24 08:07:04 +0300

    Add more test cases in test_harmonic

--- tests/test_harmonic.cpp
+++ tests/test_harmonic.cpp
@@ -17,12 +17,38 @@ using namespace topa;
 using namespace topa::common;
 using namespace topa::fast;
 
-TEST_CASE("Harmonic Test") {
+TEST_CASE("Harmonic Test (100)") {
+    using Solver = ShiftSolver<>;
     Pointcloud cloud =
         Pointcloud::Load(DATA_DIR "/pointclouds/100.off").value();
+    std::cout << "Pointcloud size = " << cloud.Size() << '\n';
     auto complex = FilteredComplex<FullTreeOpt>::From(cloud, FullVR());
     auto pairs = DoubleTwist<SparseMatrix, BitTreeHeap>::Compute(complex);
     auto harmonic_cycles =
-        Harmonic<ShiftSolver<>, SVDSeparator, 100>::Compute(complex, pairs);
+        Harmonic<Solver, SVDSeparator, 100>::Compute(complex, pairs);
+    detail::PrintCycles(harmonic_cycles);
+}
+
+TEST_CASE("Harmonic Test (325)") {
+    using Solver = ShiftSolver<25, 50, 15000, 6>;
+    Pointcloud cloud =
+        Pointcloud::Load(DATA_DIR "/pointclouds/325.off").value();
+    std::cout << "Pointcloud size = " << cloud.Size() << '\n';
+    auto complex = FilteredComplex<FullTreeOpt>::From(cloud, FullVR());
+    auto pairs = DoubleTwist<SparseMatrix, BitTreeHeap>::Compute(complex);
+    auto harmonic_cycles =
+        Harmonic<Solver, SVDSeparator, 1>::Compute(complex, pairs);
+    detail::PrintCycles(harmonic_cycles);
+}
+
+TEST_CASE("Harmonic Test (525)") {
+    using Solver = ShiftSolver<25, 50, 15000, 6>;
+    Pointcloud cloud =
+        Pointcloud::Load(DATA_DIR "/pointclouds/525.off").value();
+    std::cout << "Pointcloud size = " << cloud.Size() << '\n';
+    auto complex = FilteredComplex<FullTreeOpt>::From(cloud, FullVR());
+    auto pairs = DoubleTwist<SparseMatrix, BitTreeHeap>::Compute(complex);
+    auto harmonic_cycles =
+        Harmonic<Solver, SVDSeparator, 10>::Compute(complex, pairs);
     detail::PrintCycles(harmonic_cycles);
 }
