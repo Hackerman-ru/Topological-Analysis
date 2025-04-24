8f4823030f9ab19534f7e7a310c083f53b6e6af2
Alexia <9613983333@mail.ru>
2025-04-24 13:06:56 +0300

    Add self-adjoint solvers and comparison with shift-and-invert solver

---
+++ include/topa/fast/sa_solver.hpp
@@ -0,0 +1,43 @@
+#pragma once
+
+#include "models/solver.hpp"
+
+#include <eigen3/Eigen/Eigenvalues>
+
+namespace topa::fast {
+
+template <std::size_t precision = 3>
+class SelfAdjointSolver final
+    : public models::Solver<SelfAdjointSolver<precision>> {
+    using VectorF = Eigen::VectorXf;
+    using MatrixF = Eigen::MatrixXf;
+    static constexpr float tol = std::pow(1e-1, precision);
+
+   public:
+    static MatrixF FindKernel(const EigenMatrix& matrix) {
+        const int n = matrix.rows();
+        Eigen::SelfAdjointEigenSolver<EigenMatrix> eigs(matrix);
+        if (eigs.info() != Eigen::Success) {
+            return {};
+        }
+
+        VectorF eigenvalues = eigs.eigenvalues();
+        MatrixF eigenvectors = eigs.eigenvectors();
+
+        std::vector<size_t> zero_indices;
+        for (auto i = 0; i < eigenvalues.size(); ++i) {
+            if (std::abs(eigenvalues[i]) < tol) {
+                zero_indices.push_back(i);
+            }
+        }
+
+        MatrixF filtered_eigenvectors(n, zero_indices.size());
+        for (size_t j = 0; j < zero_indices.size(); ++j) {
+            filtered_eigenvectors.col(j) = eigenvectors.col(zero_indices[j]);
+        }
+
+        return filtered_eigenvectors;
+    }
+};
+
+}  // namespace topa::fast
---
+++ tests/compare_shift_and_adjoint_solvers.cpp
@@ -0,0 +1,36 @@
+#include "fast/sai_solver.hpp"
+#include "fast/sa_solver.hpp"
+
+#include "common/pointcloud.hpp"
+#include "common/filtered_complex.hpp"
+#include "common/svd_separator.hpp"
+#include "common/detail/harmonic_printer.hpp"
+#include "fast/harmonic.hpp"
+#include "fast/full_vr.hpp"
+#include "fast/full_tree_opt.hpp"
+#include "fast/double_twist.hpp"
+#include "fast/bit_tree_heap.hpp"
+#include "fast/sparse_matrix.hpp"
+
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/benchmark/catch_benchmark.hpp>
+
+using namespace topa;
+using namespace topa::common;
+using namespace topa::fast;
+
+TEST_CASE("Solver comparisons") {
+    Pointcloud cloud =
+        Pointcloud::Load(DATA_DIR "/pointclouds/100.off").value();
+    std::cout << "Pointcloud size = " << cloud.Size() << '\n';
+    auto complex = FilteredComplex<FullTreeOpt>::From(cloud, FullVR());
+    auto pairs = DoubleTwist<SparseMatrix, BitTreeHeap>::Compute(complex);
+
+    BENCHMARK("SelfAdjointSolver") {
+        Harmonic<SelfAdjointSolver<>, SVDSeparator, 100>::Compute(complex,
+                                                                  pairs);
+    };
+    BENCHMARK("ShiftSolver") {
+        Harmonic<ShiftSolver<>, SVDSeparator, 100>::Compute(complex, pairs);
+    };
+}
