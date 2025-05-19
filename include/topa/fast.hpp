6d6fb961275755a77d97321c9e4b73c0aef358bf
Alexia <9613983333@mail.ru>
2025-05-19 19:28:41 +0300

    Add headers with all content

---
+++ include/topa/fast.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "fast/bit_tree_heap.hpp"
+#include "fast/double_twist.hpp"
+#include "fast/flat_tree.hpp"
+#include "fast/full_tree_opt.hpp"
+#include "fast/full_tree.hpp"
+#include "fast/full_vr.hpp"
+#include "fast/harmonic.hpp"
+#include "fast/sa_solver.hpp"
+#include "fast/sai_solver.hpp"
+#include "fast/sparse_matrix.hpp"
+#include "fast/twist.hpp"
---
+++ include/topa/models.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "models/distance.hpp"
+#include "models/filtered_complex.hpp"
+#include "models/filtration.hpp"
+#include "models/harmonic.hpp"
+#include "models/matrix.hpp"
+#include "models/persistence_diagram.hpp"
+#include "models/pointcloud.hpp"
+#include "models/pos_heap.hpp"
+#include "models/separator.hpp"
+#include "models/simplex_tree.hpp"
+#include "models/solver.hpp"
